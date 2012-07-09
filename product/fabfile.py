385b9d5ec47e9c202a01e14caa057f8ffdb753f5
Ernest Gibbons <attorney@large.com>
2012-07-09 03:28:06 -0700

    can't make a blanket without fabric THAT IS THE JOKE THE JOKE IS THAT THE MODULE IS NAMED FABRIC

---
+++ product/fabfile.py
@@ -0,0 +1,24 @@
+# putting this parallel to the manager allows default cli behavior until I work out a gui
+# or it would, if i had the namespace of 'config' correct
+
+from fabric.operations import sudo
+from fabric.api import env
+from fabric.api import settings
+
+from config import config
+
+env.user = config.blanketUser
+
+def fix_time():
+    sudo('grep -q "pool.ntp.org" /etc/ntp/step-tickers || echo "pool.ntp.org" >> /etc/ntp/step-tickers')
+    sudo('/sbin/service ntpd restart')
+    sudo('/sbin/hwclock -w')
+
+def fix_puppet():
+    sudo('/usr/sbin/puppetd --onetime --no-daemonize -v')
+    sudo('/usr/local/bin/puppet-clientmon.sh')
+
+def clean_icinga():
+    with settings(host_string=config.blanketIcinga):
+        sudo('/etc/icinga/iconfig/cleanIcinga.sh')
+        sudo('/etc/icinga/iconfig/go.sh')
