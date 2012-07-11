60a4882e80bdb354880c44fa7ca3bb25c273cc4c
Ernest Gibbons <attorney@large.com>
2012-07-11 02:11:31 -0700

    need beautifulsoup too

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@
 sudo easy_install pip
 sudo pip install cherrypy
 sudo pip install soaplib  # Mac OS X probably already has the latest soaplib
+sudo pip install beautifulsoup4
 
 # add something here to install GCC
 
