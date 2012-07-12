757177ad7bd3029dfc9ad371246242cf215cf3e2
RK Davies <magne.ignis.fatuus@gmail.com>
2012-07-11 22:11:29 -0700

    added script to get host-lists from icinga monitor

---
+++ product/get_alert_hosts.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+import re
+import sys
+import requests 
+from BeautifulSoup import BeautifulSoup
+
+try:
+    alert_arg = sys.argv[1]
+except:
+    print "Please enter a valid alert to search for"
+    print "example: 'Puppet client status'"
+    exit()
+
+try:
+    import config
+    r = requests.get(config.icinga_url, verify=False, auth=(config.username, config.password))
+except:
+    import getpass
+    username = raw_input("Username: ")
+    password = getpass.getpass()
+    icinga_url = raw_input("URL to Icinga Service Status Details: ")
+    r = requests.get(icinga_url, verify=False, auth=(username, password))
+
+html = r.content
+soup = BeautifulSoup(html)
+found_alerts = []
+hostnames = []
+
+alerts = soup.findAll(text=re.compile(alert_arg))
+
+for alert in alerts:
+    found_alerts.append(alert.findPrevious('a'))
+
+for item in found_alerts:
+    host_string = str(item)
+    if 'com&amp' in host_string:                                                                                                                                                           
+        end_of_host = host_string.index('com&amp')                                                                                                                                                
+        just_the_host = host_string[37:end_of_host] + "com"                                                                                                                   
+        hostnames.append(just_the_host)
+       
+print ','.join(hostnames)
